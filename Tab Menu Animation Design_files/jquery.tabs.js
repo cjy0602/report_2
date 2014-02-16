b384bf72f9f98a72be7b2e486637f3d14b8a0b98
sub <gjwjqzz2@naver.com>
2014-02-16 19:47:52 +0900

    Css error

--- Tab Menu Animation Design_files/jquery.tabs.js
+++ Tab Menu Animation Design_files/jquery.tabs.js
@@ -21,30 +21,53 @@
 			// 대상 참조.
 			var $this = $(this),
 				$menu = $this.find('.tab_menu'),
-				$menu_li = $menu.find('li'),
-				$menu_a = $menu_li.find('a'),
-				$contents = $this.find('.tab_contents'),
+				$menu2 = $menu.find('#1'),
+				$menu_li2 = $menu2.find('li'),
+				$menu_a2 = $menu_li2.find('a'),
+				$contents2 = $this.find('.tab_contents').find('#1'),
+				$menu3 = $menu.find("#2"),
+				$menu_li3 = $menu3.find('li'),
+				$menu_a3 = $menu_li3.find('a'),
+				$contents3 = $this.find('.tab_contents').find('#2'),
 				support_features = !Modernizr.opacity || !Modernizr.csstransitions;
-				
-			// 랜덤 인덱스 설정.	
-			if(options.random) 
-				options.start_index = Math.floor(Math.random() * $menu_li.length + 1);
 			
+
+
 			// 플러그인 대상 객체에 클래스 부여	
 			$this.addClass('tabs');
 			
 			// 처음 보여질 탭 설정.
-			$menu.add($contents)
-				.find('li:nth-child(' + options.start_index + ')').addClass('active');
+			$menu2.add($contents2)
+				.find('li:nth-child(1)').addClass('active');
+
+			$menu3.add($contents3)
+				.find('li:nth-child(1)').addClass('active');
 			
 			// opacity 미지원 브라우저에 적용.
 			if(support_features) {
 				
-				$menu_li.find('img').animate({'opacity': 0}, 10, function() {
-					$menu_li.filter('.active').find('img').animate({'opacity': 1}, 10);	
+				$menu_li2.find('img').animate({'opacity': 0}, 10, function() {
+					$menu_li2.filter('.active').find('img').animate({'opacity': 1}, 10);	
+				});
+
+				$menu_li3.find('img').animate({'opacity': 0}, 10, function() {
+					$menu_li3.filter('.active').find('img').animate({'opacity': 1}, 10);	
 				});
 				
-				$menu_a
+				$menu_a2
+					.mouseover(function() {
+						$(this)
+							.stop().animate({'padding-left': '2.2em', 'padding-right': '0.8em'}, 200)
+							.find('img').stop().animate({'opacity': 1, 'left': 6}, 200);
+					})
+					.mouseout(function() {
+						if($(this).parent().hasClass('active')) return false;
+						$(this)
+							.stop().animate({'padding-left': '1.5em', 'padding-right': '1.5em'}, 200)
+							.find('img').stop().animate({'opacity':0, 'left': 16}, 200);
+					});
+
+				$menu_a3
 					.mouseover(function() {
 						$(this)
 							.stop().animate({'padding-left': '2.2em', 'padding-right': '0.8em'}, 200)
@@ -59,7 +82,7 @@
 			};
 				
 			// $menu 내부의 a 클릭 시 이벤트 핸들링
-			$menu_a.click(function(e) {
+			$menu_a2.click(function(e) {
 			
 				// 대상 참조.
 				var $this = $(this),
@@ -69,19 +92,52 @@
 	 			if($this.parent().hasClass('active')) return;
 	 			
 	 			// $menu_link에서 active 클래스 제거
-				$menu_li.removeClass('active');
+				$menu_li2.removeClass('active');
 				
 				// 클릭한 a의 부모 li에 active 클래스 추가
 				$this.parent().addClass('active');
 				
 				// opacity 미지원 브라우저에 적용.	
 				if(support_features) {
-					$menu_li.not('.active').find('a').mouseout();
+					$menu_li2.not('.active').find('a').mouseout();
 					$(this).mouseover();
 				};
 				
 				// 부드러운 장면전환.
-				$contents.find('li')
+				$contents2.find('li')
+					.fadeTo(options.transition_time, 0, function() {
+						$(this).removeClass('active')
+							.filter(target).addClass('active').fadeTo(options.transition_time, 1);
+				});
+			
+				// 브라우저 링크 기본 동작 차단
+				e.preventDefault();
+				
+			});
+
+			$menu_a3.click(function(e) {
+			
+				// 대상 참조.
+				var $this = $(this),
+					target = $this.attr('href');
+					
+				// 활성화된 a 클릭 시, 작동하지 않도록 설정.
+	 			if($this.parent().hasClass('active')) return;
+	 			
+	 			// $menu_link에서 active 클래스 제거
+				$menu_li3.removeClass('active');
+				
+				// 클릭한 a의 부모 li에 active 클래스 추가
+				$this.parent().addClass('active');
+				
+				// opacity 미지원 브라우저에 적용.	
+				if(support_features) {
+					$menu_li3.not('.active').find('a').mouseout();
+					$(this).mouseover();
+				};
+				
+				// 부드러운 장면전환.
+				$contents3.find('li')
 					.fadeTo(options.transition_time, 0, function() {
 						$(this).removeClass('active')
 							.filter(target).addClass('active').fadeTo(options.transition_time, 1);
